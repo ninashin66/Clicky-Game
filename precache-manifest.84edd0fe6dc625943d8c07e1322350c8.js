d7f02ed042e203048272cb22220b3500018b0427
Nina Shin <48607661+ninashin66@users.noreply.github.com>
2019-11-07 19:43:57 -0700

    Updates

---
+++ precache-manifest.84edd0fe6dc625943d8c07e1322350c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b308ff322af66ed107a85ccb520ff81a",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "4cd0e5173fa48187ce5d",
+    "url": "/Clicky-Game/static/css/main.ce36d60d.chunk.css"
+  },
+  {
+    "revision": "7fc024a4fe7cc60fae2a",
+    "url": "/Clicky-Game/static/js/2.b19a2de5.chunk.js"
+  },
+  {
+    "revision": "4cd0e5173fa48187ce5d",
+    "url": "/Clicky-Game/static/js/main.ed1e68f9.chunk.js"
+  },
+  {
+    "revision": "eabcff367a27d49b89d0",
+    "url": "/Clicky-Game/static/js/runtime-main.ca802268.js"
+  }
+]);
